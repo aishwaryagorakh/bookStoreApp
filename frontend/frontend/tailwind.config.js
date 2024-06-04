d269348f883aa6698ea4455758b179698c08036f
aishwarya shinde <aishwaryagorakhshinde@gmail.com>
2024-06-05 01:14:34 +0530

    adding backend

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -1,11 +1,7 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./components/**/*.{html,js,jsx,ts,tsx}", // Include other directories if necessary
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
-
   theme: {
     extend: {},
   },
